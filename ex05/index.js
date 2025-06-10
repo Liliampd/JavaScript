ff08e09cf77ab95e4077d85a6574f67f20cd0d47
Liliam Santos <liliam.santos@pditabira.com>
2025-06-10 16:04:05 -0300

    ok

--- ex05/index.js
+++ ex05/index.js
@@ -1,16 +1,16 @@
 // Criar a função que calcula o tempo restante, e recebe uma data futura.
 
-const calcularTempoRestante =(DataFutura) => {
+const calcularTempoRestante = (dataFutura) => {
     const agora = new Date().getTime()
-    const diferencaDeDatas = DataFutura - agora;
+    const diferencaDeDatas = dataFutura - agora;
     
     const segundosDeUmMinuto = 60 * 1000;
     const segundosDeUmaHora = 60 * segundosDeUmMinuto;
     const segundosDoDia = 24 * segundosDeUmaHora;
 
-    const dias = Math.floor(diferencadedatas / segundosDoDia);
-    const horas = Math.floor((diferencaDeDatas / segundosDoDia) / segundosDeUmaHora);
-    const minutos = Math.floor((diferencaDeDatas% segundosDeUmaHora) / segundosDeUmMinuto);
+    const dias = Math.floor(diferencaDeDatas / segundosDoDia);
+    const horas = Math.floor((diferencaDeDatas % segundosDoDia) / segundosDeUmaHora);
+    const minutos = Math.floor((diferencaDeDatas % segundosDeUmaHora) / segundosDeUmMinuto);
     const segundos = Math.floor((diferencaDeDatas % segundosDeUmMinuto) / 1000);
 
     return {
